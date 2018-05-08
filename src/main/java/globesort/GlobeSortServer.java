41fc9bf0d2b286290cf68b1c0d8e8fabf258e0ff
Eric_Wenyi <ericwenyi1995@outlook.com>
2018-05-08 15:40:59 -0700

    changes to the server side

--- src/main/java/globesort/GlobeSortServer.java
+++ src/main/java/globesort/GlobeSortServer.java
@@ -91,6 +91,7 @@ public class GlobeSortServer {
             long application_startTime = System.currentTimeMillis();
             Integer[] values = req.getValuesList().toArray(new Integer[req.getValuesList().size()]);
             Arrays.sort(values);
+            System.out.println("Application time is " + (application_endTime - application_startTime));
             IntArray.Builder responseBuilder = IntArray.newBuilder();
             for(Integer val : values) {
                 responseBuilder.addValues(val);
@@ -99,7 +100,7 @@ public class GlobeSortServer {
             responseObserver.onNext(response);
             responseObserver.onCompleted();
             long application_endTime = System.currentTimeMillis();
-            System.out.println("Application time is " + (application_endTime - application_startTime));
+            
         }
     }
 }
